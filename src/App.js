df53368ec39bf6a85f3015753e90df241fa7201e
Lance Toledo <50975456+lancetoledo@users.noreply.github.com>
2023-10-06 12:01:30 -0400

    Intialized App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  let [url, setUrl] = useState('//www.balldontlie.io/api/v1/players')
+  let [players, setPlayers] = useState('')
+
+  let getPlayerId = () => {
+    axios.get(url + "?search=lebron").then(async res => {
+      console.log(res.data.data)
+    }).catch(e => {
+      console.log(e)
+    })
+  }
+
+  let getPlayerStats = () => {
+    axios.get("//www.balldontlie.io/api/v1/season_averages?season=2019&player_ids[]=237").then(async res => {
+      console.log(res.data.data, "YERR")
+    }).catch(e => {
+      console.log(e)
+    })
+  }
+
+  useEffect(() => {
+    getPlayerId()
+    getPlayerStats()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      NBA
     </div>
   );
 }
